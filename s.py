6c569edcdd8310978517595303dc7471529eddc8
PiBot0 <main.piserver@gmail.com>
2019-12-11 17:41:09 -0800

    Add Files

--- s.py
+++ s.py
@@ -6,7 +6,7 @@ while True:
 	xc = (xc + 1)
 	os.system('echo screens: ' + str(xc) + ' > _config.yml') 
 	print(xc)
-	os.system('scrot myscreen' + str(xc) + '.png')
+	os.system('scrot screenshots/myscreen' + str(xc) + '.png')
 	print('pushing')
 	os.system('git add * >> /dev/null;git commit * -m "Add Files" >> /dev/null;git push https://github.com/cole-wilson/quiz master >> /dev/null')
 	time.sleep(900)
