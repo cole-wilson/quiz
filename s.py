24602f5ed607d8008512db4ff7aa86f7e253efe7
kjsdhf <asdsafda@example.com>
2019-12-11 16:40:29 -0800

    Add Files

--- s.py
+++ s.py
@@ -6,7 +6,6 @@ while True:
 	xc = (xc + 1)
 	print(xc)
 	os.system('scrot myscreen' + str(xc) + '.png')
-	time.sleep(1)
-	if xc % 1 == 0:
-		print('pushing')
-		os.system('git add *;git commit * -m "Add Files";git push https://github.com/cole-wilson/quiz master')
+	print('pushing')
+	os.system('git add *;git commit * -m "Add Files";git push https://github.com/cole-wilson/quiz master')
+	time.sleep(900)
