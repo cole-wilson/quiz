879dd69ff716e26ba5d6ec0b0245b5ca6d596cb3
bot <main.piserver@gmail.com>
2019-12-11 17:33:18 -0800

    Add Files

--- s.py
+++ s.py
@@ -1,12 +1,13 @@
 import os, time
 
-xc =2
+xc =4
 
 while True:
 	xc = (xc + 1)
+	os.system('echo screens: ' + str(xc) + '> _config.yml') 
 	print(xc)
 	os.system('scrot myscreen' + str(xc) + '.png')
 	print('pushing')
-	os.system('git add *;git commit * -m "Add Files";git push https://github.com/cole-wilson/quiz master')
+	os.system('git add * >> /dev/null;git commit * -m "Add Files" >> /dev/null;git push https://github.com/cole-wilson/quiz master >> /dev/null')
 	time.sleep(900)
 	print('waiting.....\n\n\n\n\...\n\n\n\n')
